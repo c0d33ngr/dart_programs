f10bedc91f8d5847a26cbaeb8dc77f932834eedf
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2022-10-09 05:53:11 +0100

    Program to swap two integer variable with third variable

---
+++ Practice1/exercise8.dart
@@ -0,0 +1,15 @@
+/** Program to swap two numbers with third variable **/
+
+void main() {
+	int a = 1;
+	int b = 2;
+	int temp;
+	
+	print("a is $a and b is $b");
+	
+	temp = a;
+	a = b;
+	b = a;
+	
+	print("a is now $a and b is now $b");
+}
